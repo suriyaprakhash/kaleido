9468ad34153711f85d1b65606091ee6185317af2
suriya-wsl <suriyaprakhash@gmail.com>
2024-03-09 15:12:28 -0500

    updated css for the drag and drop

--- src/app/components/dragAndDrop.tsx
+++ src/app/components/dragAndDrop.tsx
@@ -4,10 +4,12 @@ export default function DragAndDrop() {
   const [dragActive, setDragActive] = useState<boolean>(false);
   const inputRef = useRef<any>(null);
   const [files, setFiles] = useState<any>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   function handleChange(e: any) {
     e.preventDefault();
     console.log("File has been added");
+    setErrorMessage('');
     if (e.target.files && e.target.files[0]) {
       console.log(e.target.files);
       for (let i = 0; i < e.target.files["length"]; i++) {
@@ -19,17 +21,28 @@ export default function DragAndDrop() {
   async function handleSubmitFile() {
     if (files.length === 0) {
       // no file has been submitted
+      setFiles([]);
+      setErrorMessage('No files attached');
     } else {
       // write submit logic here
       // fs.readFileSync(files.pat)
-      const jsonData = await readFile(files[0])
-      console.log(jsonData)
+      if (files.length > 1) {
+        setFiles([]);
+        setErrorMessage('More than one file attached');
+      } else if (files[0].type === 'application/json') {
+        const jsonData = await readFile(files[0])
+        console.log(jsonData)
+        setErrorMessage('');
+      } else {
+        setFiles([]);
+        setErrorMessage('Not a json file');
+      }
     }
   }
 
   const readFile = async (file: File): Promise<any | undefined> => {
     const reader = new FileReader();
-  
+
     return new Promise((resolve, reject) => {
       reader.onload = (event) => {
         const content = event.target?.result as string;
@@ -41,50 +54,51 @@ export default function DragAndDrop() {
           reject(error); // Reject promise with error
         }
       };
-  
+
       reader.onerror = (error) => {
         console.error('Error reading file:', error);
         reject(error); // Reject promise with error
       };
-  
+
       reader.readAsText(file);
     });
   };
 
-  function handleDrop(e: any) {
-    e.preventDefault();
-    e.stopPropagation();
+  function handleDrop(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
     setDragActive(false);
-    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-      for (let i = 0; i < e.dataTransfer.files["length"]; i++) {
-        setFiles((prevState: any) => [...prevState, e.dataTransfer.files[i]]);
+    if (event.dataTransfer?.files && event.dataTransfer.files[0]) {
+      for (let i = 0; i < event.dataTransfer.files["length"]; i++) {
+        setFiles((prevState: any) => [...prevState, event.dataTransfer?.files[i]]);
       }
     }
   }
 
-  function handleDragLeave(e: any) {
-    e.preventDefault();
-    e.stopPropagation();
+  function handleDragLeave(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
     setDragActive(false);
   }
 
-  function handleDragOver(e: any) {
-    e.preventDefault();
-    e.stopPropagation();
+  function handleDragOver(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
     setDragActive(true);
   }
 
-  function handleDragEnter(e: any) {
-    e.preventDefault();
-    e.stopPropagation();
+  function handleDragEnter(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
     setDragActive(true);
   }
 
-  function removeFile(fileName: any, idx: any) {
+  function removeFile(fileName: string, idx: number) {
     const newArr = [...files];
     newArr.splice(idx, 1);
     setFiles([]);
     setFiles(newArr);
+    setErrorMessage('');
   }
 
   function openFileExplorer() {
@@ -93,11 +107,12 @@ export default function DragAndDrop() {
   }
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center">
+      {/* <div className="flex flex-row"> */}
       <form
-        className={`${
-          dragActive ? "bg-blue-400" : "bg-blue-100"
-        }  p-4 w-1/3 rounded-lg  min-h-[10rem] text-center flex flex-col items-center justify-center`}
+        className={`${dragActive ? "bg-blue-400" : "bg-blue-100"
+          }  p-8 w-2/3 rounded-lg  min-h-[10rem] text-center flex flex-col items-center justify-center 
+          hover:bg-blue-200 hover:animate-pulse`}
         onDragEnter={handleDragEnter}
         onSubmit={(e) => e.preventDefault()}
         onDrop={handleDrop}
@@ -110,43 +125,40 @@ export default function DragAndDrop() {
           className="hidden"
           ref={inputRef}
           type="file"
-          multiple={true}
+          multiple={false}
           onChange={handleChange}
-          accept=".xlsx,.xls,image/*,.doc, .docx,.ppt, .pptx,.txt,.pdf"
+          accept=".json"
         />
 
-        <p>
-          Drag & Drop files or{" "}
-          <span
-            className="font-bold text-blue-600 cursor-pointer"
-            onClick={openFileExplorer}
-          >
-            <u>Select files</u>
-          </span>{" "}
+        <p onClick={openFileExplorer}>
+          Drag & Drop files or
+          Select files
           to upload
         </p>
+      </form>
 
-        <div className="flex flex-col items-center p-3">
-          {files.map((file: any, idx: any) => (
-            <div key={idx} className="flex flex-row space-x-5">
-              <span>{file.name}</span>
-              <span
-                className="text-red-500 cursor-pointer"
-                onClick={() => removeFile(file.name, idx)}
-              >
-                remove
-              </span>
-            </div>
-          ))}
-        </div>
-
-        <button
-          className="bg-black rounded-lg p-2 mt-3 w-auto"
+      <button
+          className="bg-teal-600 rounded-lg p-5 mt-3 w-auto"
           onClick={handleSubmitFile}
         >
-          <span className="p-2 text-white">Submit</span>
+          <span className="p-2 text-white">Process</span>
         </button>
-      </form>
+
+      {errorMessage.length == 0 ? null :
+          <div className="bg-red-700 text-white opacity-50 rounded-xl p-2 mt-3 w-auto">{errorMessage}</div>
+        }
+
+      {files.map((file: File, idx: number) => (
+        <div key={idx} className="flex flex-row space-x-5">
+          <span>{file.name}</span>
+          <span
+            className="text-red-500 cursor-pointer"
+            onClick={() => removeFile(file.name, idx)}
+          >
+            remove
+          </span>
+        </div>
+      ))}
     </div>
   );
 }
