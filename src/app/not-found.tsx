29c5ebc134a6c5d422e4f37dcc5eb672e70ceb00
suriya-wsl <suriyaprakhash@gmail.com>
2024-03-28 07:52:34 -0400

    updated not found, version and removed next svgs

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <section className="h-[80vh] grid grid-cols gap-4 content-center">
+        <div className="text-center text-orange-300 text-3xl">404 | Content Not Found</div>
+    </section>
+    
+  )
+}
+
+export default NotFound
