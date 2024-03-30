b1d1311be227cf462b32ed179325c31c65d7d063
suriya-wsl <suriyaprakhash@gmail.com>
2024-03-30 12:54:44 -0400

    updated ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* <body className={inter.className}> */}
+
       <body className="">
+        <div>
+          <Script
+            strategy="afterInteractive" // Recommended for GTM
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HNBFN3E2FT');
+          `,
+            }}
+          />
+          {/* Your component content */}
+        </div>
         <Navbar />
         <main>
           {children}
